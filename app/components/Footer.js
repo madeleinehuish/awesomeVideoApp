102996c94e52d46a838530ef1de94d527f96c5b5
carolynkunz <carolyn.delbeccaro@gmail.com>
2017-03-30 16:36:03 -0700

    footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <h1>Footer</h1>
+    <div style={styles.footer}>
+      <div className="container">
+        <p style={styles.footerText}>	&#169; MH & CK</p>
+      </div>
+    </div>
   )
 };
 
 export default Footer;
+
+const styles = {
+  footer: {
+    backgroundColor: '#4C4E60'
+  },
+  footerText: {
+    fontFamily: 'Overpass Mono',
+    color: '#D9DAE0',
+    padding: '.5em',
+    fontSize: '14px'
+  }
+}
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoTest from './VideoTest';
 
 const Main = () => {
   return (
     <div style={styles.main}>
       <div className="container">
-        <div className= "row col-sm-6">
-          <div className="videoScreen col-sm-6" style={styles.videoScreen}>
+        <div className= "row">
+          {/* <div className="videoScreen col-sm-6" style={styles.videoScreen}>
             <p style={styles.videoScreenText}>Upload your video here</p>
-          </div>
-          <SearchBar/>
+          </div> */}
+          <VideoTest />
+          <SearchBar />
         </div>
       </div>
     </div>
@@ -21,20 +23,20 @@ export default Main;
 const styles = {
   main: {
     backgroundColor: '#D9DAE0',
-    height: '100%',
+    height: 600,
+    // height: '600px',
     padding: 20
   },
 
-  videoScreen: {
-    height: '35em',
-    width: '35em',
-    backgroundColor: '#000',
-  },
-
-  videoScreenText: {
-    fontSize: '14px',
-    color: '#fff',
-    textAlign: 'center',
-    
-  }
+  // videoScreen: {
+  //   height: '35em',
+  //   width: '35em',
+  //   backgroundColor: '#000',
+  // },
+  //
+  // videoScreenText: {
+  //   fontSize: '14px',
+  //   color: '#fff',
+  //   textAlign: 'center',
+  // }
 }
